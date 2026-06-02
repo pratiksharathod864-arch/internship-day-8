45ba167a3f7789853bcc29d0b27181cd981f942b
pratiksharathod864-arch <pratiksharathod864@gmail.com>
2026-06-02 16:41:04 +0530

    condition

---
+++ condition.js
@@ -0,0 +1,8 @@
+let age = 10
+if (age > 5) {
+    console.log("age is greater than 5")
+}   else if (age == 5) {
+    console.log("age is equal to 5")
+} else {
+    console.log("age is less than 5")
+}
---
+++ switchcase.js
@@ -0,0 +1,25 @@
+let vowel = 'A'
+switch (vowel) {
+    case 'a':
+    case 'A':
+        console.log("vowel is a")
+        break;
+    case 'e':
+    case 'E':
+        console.log("vowel is e")
+        break;
+    case 'i':
+    case 'I':
+        console.log("vowel is i")
+        break;
+    case 'o':
+    case 'O':
+        console.log("vowel is o")
+        break;
+    case 'u':
+    case 'U':
+        console.log("vowel is u")
+        break;
+    default:
+        console.log("input is not a vowel")
+}
